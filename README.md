e000f20210ec3aad2bff399ea75d0cd62d1444c9
Lisa Han <ellesis@gmail.com>
2021-10-25 08:27:13 -0800

    Web server open port:3000

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Node Express web server
 
 npm init
 npm install express --save
+make app.js
---
+++ app.js
@@ -0,0 +1,7 @@
+var express = require('express')
+var app = express()
+
+// Web server open port:3000
+app.listen(3000, function () {
+  console.log('start! express server on port 3000')
+})
